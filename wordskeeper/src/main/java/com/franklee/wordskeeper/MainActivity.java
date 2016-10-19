c7f7958f11d1e1bfdd243bbfb6914584c25348f1
franklifeisnow <franklifeisnow@gmail.com>
2016-10-19 15:00:51 +0800

    change package

--- wordskeeper/src/main/java/com/franklee/wordskeeper/MainActivity.java
+++ wordskeeper/src/main/java/com/franklee/wordskeeper/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.wordskeeper;
+package com.franklee.wordskeeper;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
--- wordskeeper/src/test/java/com/franklee/wordskeeper/ExampleUnitTest.java
+++ wordskeeper/src/test/java/com/franklee/wordskeeper/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.wordskeeper;
+package com.franklee.wordskeeper;
 
 import org.junit.Test;
 
