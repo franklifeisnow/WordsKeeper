c3e243a9cba94a1cb037c274b264760b8c7b1260
Administrator <franklifeisnow@gmail.com>
2016-10-20 21:58:11 +0800

    translationActivity

--- wordskeeper/src/main/java/com/franklee/wordskeeper/MainActivity.java
+++ wordskeeper/src/main/java/com/franklee/wordskeeper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.franklee.wordskeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,7 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.android.volley.RequestQueue;
+import com.franklee.wordskeeper.activity.TranslationActivity;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -32,8 +34,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, TranslationActivity.class));
             }
         });
 
