7ec931cbf1fe9c69e5a39e303e509722ee005745
franklifeisnow <franklifeisnow@gmail.com>
2016-10-21 15:02:06 +0800

    save local

--- wordskeeper/src/main/java/com/franklee/wordskeeper/activity/TranslationActivity.java
+++ wordskeeper/src/main/java/com/franklee/wordskeeper/activity/TranslationActivity.java
@@ -2,6 +2,7 @@ package com.franklee.wordskeeper.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.franklee.wordskeeper.R;
 
@@ -11,5 +12,6 @@ public class TranslationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translation);
+
     }
 }
